459636a6de94d160d42e0d04adb4b45a8c34298c
Judah <judah.22@ichat.sp.edu.sg>
2023-10-24 15:19:41 +0800

    Lab 2 Python Script

---
+++ bmi.py
@@ -0,0 +1,14 @@
+def calculate_bmi(height,weight):
+    print("Height = " + str(height))
+    print("Weight = " + str(weight))
+    bmi = weight / height / height
+    print(bmi)
+    if bmi > 25.0:
+        print("Weight classification: Overweight")
+    else:
+        if bmi < 18.5:
+            print("Weight classification: Underweight")
+        else:
+            print("Weight classification: Normal Weight")
+
+calculate_bmi(1.57,20)
