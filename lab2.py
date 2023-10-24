ed748afee3ac6fead133728f565a53305fee45ca
Judah <judah.22@ichat.sp.edu.sg>
2023-10-24 16:39:40 +0800

    Version 1.2 median

--- lab2.py
+++ lab2.py
@@ -1,9 +1,6 @@
 def display_main_menu():
     print("Enter some numbers separated by commas(e.g. 5,67,32)")
 
-def calc_average():
-    print("calc_average")
-
 
 def get_user_input():
     lists = input()
@@ -15,7 +12,7 @@ def calc_average_temperature(num_list):
     for x in num_list:
         y = float(x) + float(y)
     average = y / len(num_list)
-    print(average)
+    return average
 
 def calc_min_max_temperature(num_list):
     firstloop =0
@@ -29,14 +26,27 @@ def calc_min_max_temperature(num_list):
         if float(x) < float(min):
             min = x
     list_min_max = [min,max]
-    return(list_min_max)
+    return list_min_max
+
+def calc_median_temperature(num_list):
+    num_list.sort()
+    length = len(num_list)
+    x = length % 2
+    y = float(length) / 2
+    if x == 0:
+        median = (int(num_list[int(y)]) + int(num_list[int(y)-1])) / 2
+    else:
+        median = num_list[int(y)]
+    return median
+
 
 def main():
     print("ET0735 (DevOps for AIoT) - Lab 2 - Introduction to Python")
     display_main_menu()
     num_list = get_user_input()
-    calc_average_temperature(num_list)
-    calc_min_max_temperature(num_list)
+    print(calc_average_temperature(num_list))
+    print(calc_min_max_temperature(num_list))
+    print(calc_median_temperature(num_list))
 
 if __name__ == "__main__":
     main()
